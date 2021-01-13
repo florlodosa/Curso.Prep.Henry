294f122547b0a8b778b31984ae3e122ddcadb100
florlodosa <florencialodosa@gmail.com>
2021-01-13 01:56:46 -0300

    Homework done

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,3 +1,4 @@
+
 // No cambies los nombres de las funciones.
 
 function crearUsuario() {
@@ -8,28 +9,27 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-  function Usuario(opciones){
-    this.usuario= opciones.usuario;
-    this.nombre= opciones.nombre;
-    this.email= opciones.email;
-    this.password= opciones.password;
+  function Usuario(opciones) {
+    this.usuario = opciones.usuario,
+    this.nombre = opciones.nombre,
+    this.email = opciones.email,
+    this.password = opciones.password
   }
-  Usuario.prototype.saludar= function(){
-    return "Hola, mi nombre es " + this.nombre
-  };
 
-  return Usuario;
+   Usuario.prototype.saludar = function(){
+    return "Hola, mi nombre es " + this.nombre;
+   }
 
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-  Constructor.prototype.saludar = function(){
-    return "Hello World!";
+  Constructor.prototype.saludar= function(){
+    return "Hello World!"
   }
-
 }
 
 function agregarStringInvertida() {
@@ -38,14 +38,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse = function(){
-    var stringInvertida ="";
-    for (let i = this.length; i >= 0; i--) {
-      stringInvertida = stringInvertida + this.charAt(i);
+  String.prototype.reverse = function (){
+    var invertida = "";
+    for (let i = 0; i < this.length; i++) {
+      invertida = this[i] + invertida;
     }
-    return stringInvertida;
+    return invertida;
   }
-
 }
 
 // No modificar nada debajo de esta línea
